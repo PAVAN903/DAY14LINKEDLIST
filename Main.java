617b03832e321345f3b6bd4cde3006bef3879686
PAVAN903 <navap903@gmail.com>
2023-01-06 12:46:43 +0530

    added first node to sequence

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ import java.util.LinkedList;
 public class Main {
 	public static void main(String[] args) {
 	Linkedlist ll=new Linkedlist();
-	ll.add(56);
-	ll.add(30);
-	ll.add(70);
+	//ll.add(56);
+	//ll.add(30);
+	//ll.add(70);
+	//
+	ll.addFirst(70);
+	ll.addFirst(30);
+	ll.addFirst(56);
 	ll.print();
 	}}
